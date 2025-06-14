587bf2791c17717dd4a9ce6c3367c8fd8db91551
alvarocalebe <alvarocalebee@gmail.com>
2025-06-13 22:18:12 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Verificação de login (aqui estamos usando um usuário fixo para exemplo)
+    if (username === "estherzinha.moreninha" && password === "0510") {
+        alert("Bem-vinda, meu amor!");
+        window.location.href = "home.html";  // Redireciona para a página home
+    } else {
+        alert("Usuário ou senha inválidos!");
+    }
+});
+
