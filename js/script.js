f02ac8a64bdd664a5f240487bd926ac8b50ef29a
alvarocalebe <alvarocalebee@gmail.com>
2025-06-13 23:24:39 -0300

    mudanças

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     var password = document.getElementById("password").value;
 
     // Verificação de login (aqui estamos usando um usuário fixo para exemplo)
-    if (username === "estherzinha.moreninha" && password === "0510") {
+    if (username === "estherzinha.moreninha" && password === "1005") {
         alert("Bem-vinda, meu amor!");
         window.location.href = "home.html";  // Redireciona para a página home
     } else {
